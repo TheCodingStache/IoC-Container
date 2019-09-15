5fbe2e6a252fd19c8de0fd024f9530aa4cbd4ef7
dmspallas <dmspallas@gmail.com>
2019-09-15 03:43:06 +0300

    Interface implementation

---
+++ core/src/main/java/test/project/GameImpl.java
@@ -0,0 +1,58 @@
+package test.project;
+
+public class GameImpl implements Game {
+    @Override
+    public int getNumber() {
+        return 0;
+    }
+
+    @Override
+    public int getGuess() {
+        return 0;
+    }
+
+    @Override
+    public int setGuess(int guess) {
+        return 0;
+    }
+
+    @Override
+    public int getSmallest() {
+        return 0;
+    }
+
+    @Override
+    public int getBiggest() {
+        return 0;
+    }
+
+    @Override
+    public int getRemainingGussess() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void check() {
+
+    }
+
+    @Override
+    public boolean isValidNumber() {
+        return false;
+    }
+
+    @Override
+    public boolean isGameWon() {
+        return false;
+    }
+
+    @Override
+    public boolean isGameLost() {
+        return false;
+    }
+}
